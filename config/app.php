8090fce4485b9cf078e9a6d19a10d34fbd7e24b9
Lucas Moreno <lucas.moreno.olivos@gmail.com>
2020-04-10 21:49:01 -0500

    make it better

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'key' => env('APP_KEY', 'helloWorld'),
+    //'key' => env('APP_KEY', 'helloWorld'),
 
     /*
     |--------------------------------------------------------------------------
